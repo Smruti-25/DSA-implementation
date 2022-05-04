1c6cddc0576c70ec7490be47f489202f436f3d41
sk2597 <smruti.karade25@gmail.com>
2022-05-04 22:48:31 +0530

    Revert "insert into tree"
    This reverts commit bfe1f48ab6a7cbbba26f14e0c5e279582b711b6b.

--- Trees/binarySearchTree.js
+++ Trees/binarySearchTree.js
@@ -15,21 +15,23 @@ class BinarySearchTree {
 
   insert( value ) {
     const newNode = new Node(value);
-    if(!this.root){
+    if(!this.root) {
       this.root = newNode;
-    } else {
+    }else {
       let currentNode = this.root;
-      while (true) {
-        if (value < currentNode.left) {
-          if (!currentNode.left) {
+      while(true) {
+        if( value < currentNode.value ) {
+          if( !currentNode.left ) {
             currentNode.left = newNode;
             return this;
           }
+          currentNode = currentNode.left;
         } else {
           if(!currentNode.right) {
             currentNode.right = newNode;
             return this;
           }
+          currentNode = currentNode.right;
         }
       }
     }
