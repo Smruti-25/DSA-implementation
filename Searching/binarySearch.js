3508792624c4474fad3fbdeb16f48f9b22016bb2
sk2597 <smruti.karade25@gmail.com>
2022-05-06 22:26:36 +0530

    iterative code for binary search

---
+++ Searching/binarySearch.js
@@ -0,0 +1,19 @@
+//iterative
+
+function binarysearch( array, value ){
+  let low = 0;
+  let high = array.length - 1;
+  
+  while( low <= high ) {
+    let mid = Math.ceil(( low + high ) / 2);
+    if( value < array[mid] ){
+      high = mid - 1;
+    } else if( value > array[mid] ){
+      low = mid + 1;
+    } else return mid;
+  }
+  return false;
+}
+
+const array = [0 ,1 ,2, 3, 4, 5, 6, 7];
+console.log("binarysearch", binarysearch( array, 033 ));
