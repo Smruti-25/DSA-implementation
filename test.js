2315c0cf903ad78e6e3f23fdcef9d56fd11e042c
sk2597 <smruti.karade25@gmail.com>
2023-12-05 22:20:11 +0530

    class node and linkedlist

---
+++ test.js
@@ -0,0 +1,13 @@
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+class LinkedList {
+  constructor(value){
+    this.head = new Node(value);
+    this.tail = this.head
+    this.length = 1;
+  }
+}
